c64e7b3e6986d99ca26dc9efabc000a51cfe36c4
Sami Jaber <jabersami@gmail.com>
2024-01-10 16:23:01 -0400

    update lib

--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -1,12 +1,8 @@
+"use client";
 import { safeDynamicRequire } from "./safeDynamicRequire";
 
-const ivm = safeDynamicRequire("isolated-vm") as typeof import("isolated-vm");
+const add = safeDynamicRequire("lodash/add") as typeof import("lodash");
 
 export const runIsolatedVm = () => {
-  const isolate = new ivm.Isolate({ memoryLimit: 128 });
-  const context = isolate.createContextSync();
-  const script = isolate.compileScriptSync("1 + 2");
-  const result = script.runSync(context);
-  console.log({ result });
-  isolate.dispose();
+  return add(1, 2);
 };
