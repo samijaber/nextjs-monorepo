4b12caa1666f590b2c0d1d315c7af370c68e3311
Sami Jaber <jabersami@gmail.com>
2024-03-19 13:16:01 -0300

    repro

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -1,7 +1,6 @@
+import { Add } from "@/lib/Add";
 import styles from "./page.module.css";
-import { add3 } from "@/lib/add";
 
 export default function Home() {
-  const k = add3();
-  return <main className={styles.main}>1+2={k}</main>;
+  return <main className={styles.main}>{<Add />}</main>;
 }
--- nextjs-app/src/lib/add.tsx
+++ nextjs-app/src/lib/add.tsx
@@ -1,15 +1,7 @@
 "use client";
-import { createRequire } from "node:module";
 
-let safeDynamicRequire: any = () => {};
-try {
-  safeDynamicRequire = createRequire(__dirname);
-} catch (e) {
-  safeDynamicRequire = eval("require");
-}
+import { addNums } from "./add-nums";
 
-const add = safeDynamicRequire("lodash/add");
-
-export const add3 = () => {
-  return add(1, 2);
+export const Add = () => {
+  return <div>1 + 2 = {addNums()}</div>;
 };
