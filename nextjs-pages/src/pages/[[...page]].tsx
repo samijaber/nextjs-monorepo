4143bcb4dd14bcf472984150c8e767b6ca4681a6
Sami Jaber <jabersami@gmail.com>
2024-03-19 16:43:20 -0300

    f

--- nextjs-pages/src/pages/[[...page]].tsx
+++ nextjs-pages/src/pages/[[...page]].tsx
@@ -1,11 +1,11 @@
 import { Add } from "../comp/Add";
-import { Add as AddComp } from "@mono/lib";
+import { Comp } from "@mono/lib";
 
 export default function Home() {
   return (
     <main>
       code in webapp: {<Add />}
-      code from lib: {<AddComp />}
+      code from lib: {<Comp />}
     </main>
   );
 }
