56ece5a16cd27ab47ae8cb914a645fbd1c463fa0
Sami Jaber <jabersami@gmail.com>
2024-03-20 15:30:33 -0300

    f

--- nextjs-pages/src/pages/[[...page]].tsx
+++ nextjs-pages/src/pages/[[...page]].tsx
@@ -1,4 +1,7 @@
 import { Comp } from "@mono/lib";
+import ivm from "isolated-vm";
+const isolate = new ivm.Isolate({ memoryLimit: 128 });
+const context = isolate.createContextSync();
 
 // do not prerender
 export const getServerSideProps = async () => {
