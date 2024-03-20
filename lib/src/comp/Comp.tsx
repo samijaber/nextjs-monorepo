706d45796a102d4db4555808e12cb90ddc4c9177
Sami Jaber <jabersami@gmail.com>
2024-03-20 15:45:07 -0300

    f

--- lib/src/comp/Comp.tsx
+++ lib/src/comp/Comp.tsx
@@ -1,12 +1,11 @@
 "use client";
 import React from "react";
 
-import { addNums, getEvalResult } from "./add-nums";
+import { getEvalResult } from "./add-nums";
 
 export const Comp = () => {
   return (
     <div>
-      {/* <div>1 + 2 = {addNums()}</div> */}
       <div>1 + 2 = {getEvalResult()}</div>
     </div>
   );
